adf90b8410f1461f2bc474a722f86611cfe0b325
Maria Leitão <malufln.99@gmail.com>
2025-02-26 18:23:43 -0300

    chore: start project with spring initializr

---
+++ src/test/java/com/vitalu/flop/FlopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitalu.flop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
