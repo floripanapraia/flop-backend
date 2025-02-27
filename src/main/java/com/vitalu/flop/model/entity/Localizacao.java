c6f058f079143090274162b236e38579a776fb79
Tatiana Sakuma <tati.sakuma@gmail.com>
2025-02-27 10:23:19 -0300

    feat: classes

---
+++ src/main/java/com/vitalu/flop/model/entity/Localizacao.java
@@ -0,0 +1,12 @@
+package com.vitalu.flop.model.entity;
+
+public class Localizacao {
+
+	Long idLocalizacao;
+	String idGooglePlace;
+	Double longitude;
+	Double latitude;
+	String cidade;
+	String estado;
+	
+}
---
+++ src/main/java/com/vitalu/flop/model/entity/Praia.java
@@ -0,0 +1,14 @@
+package com.vitalu.flop.model.entity;
+
+//import java.util.List;
+
+public class Praia {
+	
+	Long idPraia;
+	String nomePraia;
+	String imagem;
+	Localizacao localizacao;
+	
+//	List<Avaliacao> avaliacoes;
+//	List <Postagem> postagens;
+}
