cd17ca102bf78c7f834c6a4c776fd309ccb94f82
Tatiana Sakuma <tati.sakuma@gmail.com>
2025-03-06 10:02:14 -0300

    refactor: Classes Localizacao/ Praia/ SugerirPraia/ LocalizacaoRepository/ SugerirPraiaRepository

--- src/main/java/com/vitalu/flop/model/entity/Localizacao.java
+++ src/main/java/com/vitalu/flop/model/entity/Localizacao.java
@@ -1,7 +1,12 @@
 package com.vitalu.flop.model.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Localizacao {
 
+	@Id
 	Long idLocalizacao;
 	String idGooglePlace;
 	Double longitude;
--- src/main/java/com/vitalu/flop/model/entity/SugerirPraia.java
+++ src/main/java/com/vitalu/flop/model/entity/SugerirPraia.java
@@ -2,8 +2,13 @@ package com.vitalu.flop.model.entity;
 
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class SugerirPraia {
 	
+	@Id
 	Long idSugestao;
 	String nomePraia;
 	String bairro;
