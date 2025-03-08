0e0bdfef853e23c37842c5b8770455182cba8763
Tatiana Sakuma <tati.sakuma@gmail.com>
2025-03-08 11:27:48 -0300

    refactor: melhorias no mapeamento da entidade Praia
    - Adicionado @GeneratedValue para geração automática do idPraia.
    
    - Melhorado relacionamento com Localizacao usando @OneToOne e @JoinColumn.
    
    - Adicionados relacionamentos com Avaliacao e Postagem usando @OneToMany.
    
    - Removidos comentários e ajustes na estrutura para melhor integração com JPA.

--- src/main/java/com/vitalu/flop/model/entity/Praia.java
+++ src/main/java/com/vitalu/flop/model/entity/Praia.java
@@ -1,23 +1,36 @@
 package com.vitalu.flop.model.entity;
 
-import jakarta.persistence.Column;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import lombok.Data;
 
 //import java.util.List;
 
 @Entity
+@Data
 public class Praia {
 	
 	@Id
-	Long idPraia;
-	@Column
-	String nomePraia;
-	@Column
-	String imagem;
-	@Column
-	Localizacao localizacao;
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idPraia;
+	private String nomePraia;
+	private String imagem;
 	
-//	List<Avaliacao> avaliacoes;
-//	List <Postagem> postagens;
+	@OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "idLocalizacao", referencedColumnName = "idLocalizacao")
+	private Localizacao localizacao;
+	
+	@OneToMany(mappedBy = "praia")
+	private List<Avaliacao> avaliacoes;
+	
+	@OneToMany(mappedBy = "praia")
+	private List<Postagem> postagens;
 }
