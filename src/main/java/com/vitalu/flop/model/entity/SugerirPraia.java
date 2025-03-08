5e619d53666c031527d12920e9550f56b59887f7
Tatiana Sakuma <tati.sakuma@gmail.com>
2025-03-08 11:39:00 -0300

    refactor: melhorias na modelagem da entidade SugerirPraia
    - Adicionado @GeneratedValue(strategy = GenerationType.IDENTITY) para geração automática do idSugestao.
    
    - Criado relacionamento ManyToOne com Usuario para associar a sugestão a um usuário específico.
    
    - Adicionado @CreationTimestamp para registrar automaticamente a data de criação da sugestão.
    
    - Incluídas validações @NotNull para garantir que nomePraia, bairro e descricao sejam obrigatórios.

--- src/main/java/com/vitalu/flop/model/entity/SugerirPraia.java
+++ src/main/java/com/vitalu/flop/model/entity/SugerirPraia.java
@@ -2,18 +2,34 @@ package com.vitalu.flop.model.entity;
 
 import java.time.LocalDateTime;
 
+import org.hibernate.annotations.CreationTimestamp;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
 
 @Entity
+@Data
 public class SugerirPraia {
 	
 	@Id
-	Long idSugestao;
-	String nomePraia;
-	String bairro;
-	String descricao;
-	Boolean analisada;
-	LocalDateTime criadaEm;
-	//Usuario usuario;
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idSugestao;
+	@NotNull(message = "É obrigatório informar o nome da praia.")
+	private String nomePraia;
+	@NotNull(message = "É obrigatório informar o nome do bairro.")
+	private String bairro;
+	@NotNull(message = "É obrigatório informar a descrição da praia.")
+	private String descricao;
+	private Boolean analisada;
+	@CreationTimestamp
+	private LocalDateTime criadaEm;
+	@ManyToOne
+	@JoinColumn(name = "idUsuario")
+	private Usuario usuario;
 }
