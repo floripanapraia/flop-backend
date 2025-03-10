fde0c8147bc37ea83dda93f0682a3b0c69e092f9
Victoria Fernandes <victoriafernandesgalvao@hotmail.com>
2025-03-10 10:58:53 -0300

    feat: fix relations

--- src/main/java/com/vitalu/flop/model/entity/Localizacao.java
+++ src/main/java/com/vitalu/flop/model/entity/Localizacao.java
@@ -4,7 +4,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
 import lombok.Data;
 
 @Entity
@@ -14,13 +13,17 @@ public class Localizacao {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long idLocalizacao;
+
 	private String idGooglePlace;
+
 	private Double longitude;
+
 	private Double latitude;
+
 	private String cidade;
+
 	private String estado;
-	@OneToOne(mappedBy = "localizacao")
-	private Praia praia;
-	
-	
+
+//	private Praia praia;
+
 }
