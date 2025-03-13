a3f677a587b01870d4607a31910a3d0c82c3c73e
Maria Leitão <88951059+marialuizaleitao@users.noreply.github.com>
2025-03-12 21:20:00 -0300

    feat: find all posts method

--- src/main/java/com/vitalu/flop/service/PostagemService.java
+++ src/main/java/com/vitalu/flop/service/PostagemService.java
@@ -1,5 +1,6 @@
 package com.vitalu.flop.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -49,6 +50,10 @@ public class PostagemService {
 		}
 
 		postagemRepository.save(postagem);
+	}
+
+	public List<Postagem> pesquisarTodos() {
+		return postagemRepository.findAll();
 	}
 
 }
