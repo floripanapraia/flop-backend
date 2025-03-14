0a3f711f83ed1f27022b53526e6e3a788ee7cfff
Victoria Fernandes <victoriafernandesgalvao@hotmail.com>
2025-03-14 09:04:29 -0300

    feat: avaliacao repository

--- src/main/java/com/vitalu/flop/model/repository/AvaliacaoRepository.java
+++ src/main/java/com/vitalu/flop/model/repository/AvaliacaoRepository.java
@@ -1,14 +1,22 @@
 package com.vitalu.flop.model.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import com.vitalu.flop.model.entity.Avaliacao;
+import com.vitalu.flop.model.entity.Praia;
+import com.vitalu.flop.model.entity.Usuario;
 
 
 
 @Repository
 public interface AvaliacaoRepository extends JpaRepository<Avaliacao, Long>, JpaSpecificationExecutor<Avaliacao>  {
+
+	List<Avaliacao> findByPraia(Praia praia);
+
+	List<Avaliacao> findByUsuario(Usuario usuario);
 
 }
