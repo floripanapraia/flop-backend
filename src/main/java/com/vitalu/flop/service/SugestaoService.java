6f612e1475ba46c82c8a88530b8fc06e124bbef0
Tatiana Sakuma <tati.sakuma@gmail.com>
2025-03-22 11:15:24 -0300

    feat: create SugestaoService with CRUD methods.

--- src/main/java/com/vitalu/flop/service/SugestaoService.java
+++ src/main/java/com/vitalu/flop/service/SugestaoService.java
@@ -1,5 +1,56 @@
 package com.vitalu.flop.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+import com.vitalu.flop.exception.FlopException;
+import com.vitalu.flop.model.entity.Sugestao;
+import com.vitalu.flop.model.repository.SugestaoRepository;
+
+@Service
 public class SugestaoService {
 
+	@Autowired
+	private SugestaoRepository sugestaoRepository;
+
+	public Sugestao criarSugestao(Sugestao novaSugestao) throws FlopException {
+		return sugestaoRepository.save(novaSugestao);
+	}
+
+	public void excluirSugestao(Long sugestaoId, Long usuarioId) throws FlopException {
+
+		Sugestao sugestao = sugestaoRepository.findById(sugestaoId)
+				.orElseThrow(() -> new FlopException("Sugestão não encontrada", HttpStatus.NOT_FOUND));
+
+		if (sugestao.getUsuario().getIdUsuario().equals(usuarioId)) {
+			sugestaoRepository.deleteById(sugestaoId);
+		} else {
+			throw new FlopException("Você não pode excluir sugestões de outras pessoas!", HttpStatus.UNAUTHORIZED);
+		}
+
+	}
+
+	public Sugestao editarSugestao(Long sugestaoId, Sugestao editarSugestao) throws FlopException {
+		Sugestao existente = sugestaoRepository.findById(sugestaoId)
+				.orElseThrow(() -> new FlopException("Sugestão não encontrada!", HttpStatus.NOT_FOUND));
+
+		existente.setNomePraia(editarSugestao.getNomePraia());
+		existente.setBairro(editarSugestao.getBairro());
+		existente.setDescricao(editarSugestao.getDescricao());
+		existente.setAnalisada(editarSugestao.getAnalisada());
+
+		return sugestaoRepository.save(existente);
+	}
+
+	public Sugestao procurarPorId(Long sugestaoId) throws FlopException {
+		return sugestaoRepository.findById(sugestaoId)
+				.orElseThrow(() -> new FlopException("Esta sugestão não foi encontrada!", HttpStatus.NOT_FOUND));
+	}
+
+	public Page<Sugestao> pesquisarSugestaoFiltros(Long sugestaoId) throws FlopException {
+		// TODO
+		return null;
+	}
 }
