8d24ce3b144d9c9cfd567994d4b2d857d77a212f
Tatiana Sakuma <tati.sakuma@gmail.com>
2025-03-28 10:30:08 -0300

    feat: add endpoint to fetch Praia by ID in controller

--- src/main/java/com/vitalu/flop/controller/PraiaController.java
+++ src/main/java/com/vitalu/flop/controller/PraiaController.java
@@ -3,9 +3,10 @@ package com.vitalu.flop.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +21,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 @RequestMapping(path = "/praia")
 @CrossOrigin(origins = { "http://localhost:4200" }, maxAge = 3600)
 public class PraiaController {
-	
+
 	@Autowired
 	private PraiaService praiaService;
 
@@ -30,5 +31,11 @@ public class PraiaController {
 	public List<Praia> pesquisarTodos() throws FlopException {
 		return praiaService.pesquisarPraiaTodas();
 	}
-	
+
+	@Operation(summary = "Pesquisar praia por ID.", description = "Busca uma praia específica através do seu ID.")
+	@GetMapping(path = "/{idPraia}")
+	public ResponseEntity<Praia> pesquisarPraiasId(@PathVariable("idPraia") Long praiaId) throws FlopException {
+		Praia praia = praiaService.pesquisarPraiasId(praiaId);
+		return ResponseEntity.ok(praia);
+	}
 }
