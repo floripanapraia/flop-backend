27303bdc63b5248814f708fea1a999b61e60db1a
Tatiana Sakuma <tati.sakuma@gmail.com>
2025-04-13 18:27:02 -0300

    feat: update  PraiaDTO to include fields for condition count, post messages and images.

--- src/main/java/com/vitalu/flop/model/dto/PraiaDTO.java
+++ src/main/java/com/vitalu/flop/model/dto/PraiaDTO.java
@@ -1,10 +1,22 @@
 package com.vitalu.flop.model.dto;
 
+import java.util.List;
+import java.util.Map;
+
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PraiaDTO {
+	private Long idPraia;
 	private String nomePraia;
-    private String imagem;
-    private Long idLocalizacao;
+	private String imagem;
+	private Long idLocalizacao;
+	private List<String> mensagensPostagens;
+	private List<String> imagensPostagens;
+	private Map<String, Integer> condicoesAvaliacoes;
+
 }
