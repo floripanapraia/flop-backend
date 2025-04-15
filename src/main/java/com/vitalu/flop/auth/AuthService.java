8c5c796f30a7eb8c0b49d13c30b6233062c60448
Maria Leitão <88951059+marialuizaleitao@users.noreply.github.com>
2025-04-15 00:25:22 -0300

    feat: auth service config class

---
+++ src/main/java/com/vitalu/flop/auth/AuthService.java
@@ -0,0 +1,50 @@
+package com.vitalu.flop.auth;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.stereotype.Service;
+
+import com.vitalu.flop.exception.FlopException;
+import com.vitalu.flop.model.entity.Usuario;
+import com.vitalu.flop.model.repository.UsuarioRepository;
+
+@Service
+public class AuthService {
+
+	private final JwtService jwtService;
+
+	@Autowired
+	private UsuarioRepository userRepository;
+
+	public AuthService(JwtService jwtService) {
+		this.jwtService = jwtService;
+	}
+
+	public String authenticate(Authentication authentication) {
+		return jwtService.generateToken(authentication);
+	}
+
+	public Usuario getAuthenticatedUser() throws FlopException {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		Usuario authenticatedUser = null;
+
+		if (authentication != null && authentication.isAuthenticated()) {
+			Object principal = authentication.getPrincipal();
+
+			Jwt jwt = (Jwt) principal;
+			String login = jwt.getClaim("sub");
+
+			authenticatedUser = userRepository.findByEmail(login)
+					.orElseThrow(() -> new FlopException("Usuário não encontrado.", HttpStatus.BAD_REQUEST));
+		}
+
+		if (authenticatedUser == null) {
+			throw new FlopException("Usuário não encontrado.", HttpStatus.BAD_REQUEST);
+		}
+
+		return authenticatedUser;
+	}
+}
