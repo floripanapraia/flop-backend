a910db56677aa422cce6d05d18aae98e01fc5521
Maria Leitão <88951059+marialuizaleitao@users.noreply.github.com>
2025-04-16 13:47:49 -0300

    feat: toEntity conversion method on userDTO

--- src/main/java/com/vitalu/flop/model/dto/UsuarioDTO.java
+++ src/main/java/com/vitalu/flop/model/dto/UsuarioDTO.java
@@ -1,7 +1,11 @@
 package com.vitalu.flop.model.dto;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+
+import com.vitalu.flop.model.entity.Usuario;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import lombok.Data;
 
@@ -9,7 +13,7 @@ import lombok.Data;
 public class UsuarioDTO {
 	private Long idUsuario;
 	private String nome;
-	private String userName;
+	private String username;
 	private String senha;
 	private String email;
 	private boolean isAdmin;
@@ -20,4 +24,24 @@ public class UsuarioDTO {
 	private List<Long> avaliacaoIds;
 	private List<Long> postagemIds;
 	private List<Long> sugerirPraiaIds;
+
+	public Usuario toEntity(PasswordEncoder encoder) {
+		Usuario usuario = new Usuario();
+		usuario.setIdUsuario(this.idUsuario);
+		usuario.setNome(this.nome);
+		usuario.setUsername(this.username);
+		if (this.senha != null && !this.senha.isEmpty()) {
+			usuario.setSenha(encoder.encode(this.senha));
+		}
+		usuario.setEmail(this.email);
+		usuario.setAdmin(this.isAdmin);
+		usuario.setFotoPerfil(this.fotoPerfil);
+		usuario.setBloqueado(this.bloqueado);
+
+		usuario.setAvaliacao(new ArrayList<>());
+		usuario.setPostagem(new ArrayList<>());
+		usuario.setSugestoes(new ArrayList<>());
+
+		return usuario;
+	}
 }
