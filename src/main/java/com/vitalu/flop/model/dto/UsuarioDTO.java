aefe2d4387b0b07c60cfc07206f935183f1a7e3a
Maria Leitão <88951059+marialuizaleitao@users.noreply.github.com>
2025-04-18 22:22:44 -0300

    refactor: remove password from userDTO

--- src/main/java/com/vitalu/flop/model/dto/UsuarioDTO.java
+++ src/main/java/com/vitalu/flop/model/dto/UsuarioDTO.java
@@ -1,19 +1,16 @@
 package com.vitalu.flop.model.dto;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
-
-import com.vitalu.flop.model.entity.Usuario;
 
 import lombok.Data;
 
 @Data
 public class UsuarioDTO {
+
 	private Long idUsuario;
 	private String nome;
 	private String username;
-	private String senha;
 	private String email;
 	private boolean isAdmin;
 	private LocalDateTime dataCriacao;
