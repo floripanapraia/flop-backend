5879826c3bec80a0ed913c0524f82b29c4e0262b
Maria Leitão <88951059+marialuizaleitao@users.noreply.github.com>
2025-06-11 10:31:48 -0300

    feat: add isBloqueado and isAdmin attributes to user DTO class

--- src/main/java/com/vitalu/flop/model/dto/UsuarioDTO.java
+++ src/main/java/com/vitalu/flop/model/dto/UsuarioDTO.java
@@ -12,11 +12,10 @@ public class UsuarioDTO {
 	private String nome;
 	private String nickname;
 	private String email;
-	private boolean isAdmin;
+	private int isAdmin;
 	private LocalDateTime dataCriacao;
 	private String fotoPerfil;
-	private boolean bloqueado;
-	// private Localizacao localizacao;
+	private int isBloqueado;
 	private List<Long> avaliacaoIds;
 	private List<Long> postagemIds;
 	private List<Long> sugerirPraiaIds;
