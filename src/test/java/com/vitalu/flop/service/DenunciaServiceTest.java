d69bdfab4d08137ea9f72cd851d3c79d843e648c
Maria Leitão <88951059+marialuizaleitao@users.noreply.github.com>
2025-06-24 00:53:47 -0300

    feat: add test for successful deletion of Denuncia by author

--- src/test/java/com/vitalu/flop/service/DenunciaServiceTest.java
+++ src/test/java/com/vitalu/flop/service/DenunciaServiceTest.java
@@ -126,4 +126,18 @@ class DenunciaServiceTest {
 		verify(denunciaRepository, never()).save(any());
 	}
 
+	// Testes para o método excluir()
+    @Test
+    @DisplayName("Deve excluir denúncia quando o usuário for o autor")
+    void testExcluir_QuandoUsuarioForAutor_DeveDeletarDenuncia() throws FlopException {
+        // Arrange
+        Long idUsuarioAutor = denunciaValida.getUsuarioDenunciador().getIdUsuario();
+        when(denunciaRepository.findById(denunciaValida.getIdDenuncia())).thenReturn(Optional.of(denunciaValida));
+        
+        // Act
+        denunciaService.excluir(denunciaValida.getIdDenuncia(), idUsuarioAutor);
+        
+        // Assert
+        verify(denunciaRepository, times(1)).deleteById(denunciaValida.getIdDenuncia());
+    }
 }
