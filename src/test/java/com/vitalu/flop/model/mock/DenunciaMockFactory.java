912c734566341099e64a0030952ee7d1fff42fb5
Maria Leitão <88951059+marialuizaleitao@users.noreply.github.com>
2025-06-24 00:45:41 -0300

    feat: add methods to create specific and rejected Denuncia instances

--- src/test/java/com/vitalu/flop/model/mock/DenunciaMockFactory.java
+++ src/test/java/com/vitalu/flop/model/mock/DenunciaMockFactory.java
@@ -3,6 +3,8 @@ package com.vitalu.flop.model.mock;
 import java.time.LocalDateTime;
 
 import com.vitalu.flop.model.entity.Denuncia;
+import com.vitalu.flop.model.entity.Postagem;
+import com.vitalu.flop.model.entity.Usuario;
 import com.vitalu.flop.model.enums.MotivosDenuncia;
 import com.vitalu.flop.model.enums.StatusDenuncia;
 
@@ -35,6 +37,37 @@ public class DenunciaMockFactory {
 		denuncia.setIdDenuncia(2L);
 		denuncia.setStatus(StatusDenuncia.ACEITA);
 		denuncia.setMotivo(MotivosDenuncia.SPAM_PROPAGANDA);
+		return denuncia;
+	}
+
+	/**
+	 * Cria uma denúncia com status RECUSADA.
+	 *
+	 * @return uma instância de {@link Denuncia} com status RECUSADA.
+	 */
+	public static Denuncia criarDenunciaRecusada() {
+		Denuncia denuncia = criarDenunciaPendente();
+		denuncia.setIdDenuncia(3L);
+		denuncia.setStatus(StatusDenuncia.RECUSADA);
+		denuncia.setMotivo(MotivosDenuncia.INCORRETO);
+		return denuncia;
+	}
+
+	/**
+	 * Cria uma denúncia para uma postagem e um denunciador específicos.
+	 *
+	 * @param postagem    A postagem a ser denunciada.
+	 * @param denunciador O usuário que está fazendo a denúncia.
+	 * @return uma instância de {@link Denuncia} associada aos parâmetros.
+	 */
+	public static Denuncia criarDenunciaEspecifica(Postagem postagem, Usuario denunciador) {
+		Denuncia denuncia = new Denuncia();
+		denuncia.setIdDenuncia(4L);
+		denuncia.setUsuarioDenunciador(denunciador);
+		denuncia.setPostagem(postagem);
+		denuncia.setCriadoEm(LocalDateTime.now());
+		denuncia.setMotivo(MotivosDenuncia.VIOLACAO_PRIVACIDADE);
+		denuncia.setStatus(StatusDenuncia.PENDENTE);
 		return denuncia;
 	}
 }
