4da3b6c0e0690bc078246dbce9717eb819d0a8df
Maria Leitão <88951059+marialuizaleitao@users.noreply.github.com>
2025-06-24 00:44:54 -0300

    feat: add DenunciaMockFactory for creating mock Denuncia instances

---
+++ src/test/java/com/vitalu/flop/model/mock/DenunciaMockFactory.java
@@ -0,0 +1,28 @@
+package com.vitalu.flop.model.mock;
+
+import java.time.LocalDateTime;
+
+import com.vitalu.flop.model.entity.Denuncia;
+import com.vitalu.flop.model.enums.MotivosDenuncia;
+import com.vitalu.flop.model.enums.StatusDenuncia;
+
+public class DenunciaMockFactory {
+
+	/**
+	 * Cria uma denúncia padrão com status PENDENTE. O denunciador é um "usuário
+	 * admin" e a postagem é a "postagem padrão".
+	 *
+	 * @return uma instância de {@link Denuncia}.
+	 */
+	public static Denuncia criarDenunciaPendente() {
+		Denuncia denuncia = new Denuncia();
+		denuncia.setIdDenuncia(1L);
+		denuncia.setUsuarioDenunciador(UsuarioMockFactory.criarUsuarioAdmin());
+		denuncia.setPostagem(PostagemMockFactory.criarPostagemPadrao());
+		denuncia.setCriadoEm(LocalDateTime.now());
+		denuncia.setMotivo(MotivosDenuncia.INADEQUADO);
+		denuncia.setStatus(StatusDenuncia.PENDENTE);
+		return denuncia;
+	}
+
+}
